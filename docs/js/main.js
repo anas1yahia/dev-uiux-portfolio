7208b16ecc4b9ffe5eafcfc5d411d4f73818dfb4
anas yahia <anas.yahya42@gmail.com>
2025-02-12 23:15:32 +0100

    update api

--- docs/js/main.js
+++ docs/js/main.js
@@ -495,10 +495,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // GitHub API Integration
+
 const GITHUB_USERNAME = 'anas1yahia';
 async function fetchGitHubRepos() {
   const projectsGrid = document.getElementById('projects-grid');
-  const GITHUB_TOKEN = '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'; // Replace with your GitHub token
+  const GITHUB_TOKEN = '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'; // Replace with your GitHub token
 
   try {
     projectsGrid.innerHTML = `<div class="flex justify-center py-8 col-span-full">
